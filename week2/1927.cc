7af70a9411214fe831532735a96a7a719eb33876
2023056353 <kimkijea7619@hanyang.ac.kr>
2024-01-11 21:07:27 +0900

    Min_Heap

---
+++ week2/1927.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+priority_queue<int,vector<int>,greater<int>> pq;
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int n;
+    cin >> n;
+    for(int i = 0 ; i < n;i++){
+        int a;
+        cin >> a;
+        if(a == 0){
+            if(pq.empty()){
+                cout << "0" << "\n";
+            }
+            else{
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }else{
+            pq.push(a);
+        }
+    }
+    return 0;
+}
