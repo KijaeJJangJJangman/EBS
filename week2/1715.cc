bb74fd9ee6c00b1e641159dc2ebef1d65cf5d0ba
2023056353 <kimkijea7619@hanyang.ac.kr>
2024-01-14 23:17:00 +0900

    priority_queue util

---
+++ week2/1715.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+priority_queue<int,vector<int>,greater<int>> pq;
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);
+    int n;
+    cin >> n;
+    int total = 0;
+    for(int i = 0; i < n; i++){
+        int a;
+        cin >> a;
+        pq.push(a);
+    }
+    while(pq.size() > 1){
+        int n1, n2;
+        n1 = pq.top();
+        pq.pop();
+        n2 = pq.top();
+        pq.pop();
+        total += (n1+n2);
+        pq.push(n1+n2);
+    }
+    cout << total;
+    return 0;
+}
